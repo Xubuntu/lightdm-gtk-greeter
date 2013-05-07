42559015fb015e09ec88b379dcc1e54c6ba7d67a
Sean Davis <smd.seandavis@gmail.com>
2013-05-07 19:55:25 -0400

    Add primary/secondary text and padding to shutdown/restart dialogs

--- src/lightdm-gtk-greeter.c
+++ src/lightdm-gtk-greeter.c
@@ -951,11 +951,13 @@ restart_cb (GtkWidget *widget, LightDMGreeter *greeter)
                                      GTK_DIALOG_MODAL,
                                      GTK_MESSAGE_OTHER,
                                      GTK_BUTTONS_NONE,
-                                     "%s", _("Are you sure you want to close all programs and restart the computer?"));
+                                     "%s", _("Restart"));
+    gtk_message_dialog_format_secondary_text(GTK_MESSAGE_DIALOG(dialog), "%s", _("Are you sure you want to close all programs and restart the computer?"));
     g_signal_connect (G_OBJECT (dialog), "size-allocate", G_CALLBACK (login_window_size_allocate), NULL);
     gtk_message_dialog_set_image(GTK_MESSAGE_DIALOG(dialog), image);
     gtk_dialog_add_buttons (GTK_DIALOG (dialog), _("Return To Login"), FALSE, _("Restart"), TRUE, NULL);
     gtk_widget_set_name(dialog, "login_window");
+    gtk_container_set_border_width(GTK_CONTAINER(dialog), 18);
     gtk_widget_show_all (dialog);
     center_window (GTK_WINDOW (dialog));
 
@@ -985,11 +987,13 @@ shutdown_cb (GtkWidget *widget, LightDMGreeter *greeter)
                                      GTK_DIALOG_MODAL,
                                      GTK_MESSAGE_OTHER,
                                      GTK_BUTTONS_NONE,
-                                     "%s", _("Are you sure you want to close all programs and shutdown the computer?"));
+                                     "%s", _("Shutdown"));
+    gtk_message_dialog_format_secondary_text(GTK_MESSAGE_DIALOG(dialog), "%s", _("Are you sure you want to close all programs and shutdown the computer?"));
     g_signal_connect (G_OBJECT (dialog), "size-allocate", G_CALLBACK (login_window_size_allocate), NULL);
     gtk_message_dialog_set_image(GTK_MESSAGE_DIALOG(dialog), image);
     gtk_dialog_add_buttons (GTK_DIALOG (dialog), _("Return To Login"), FALSE, _("Shutdown"), TRUE, NULL);
     gtk_widget_set_name(dialog, "login_window");
+    gtk_container_set_border_width(GTK_CONTAINER(dialog), 18);
     gtk_widget_show_all (dialog);
     center_window (GTK_WINDOW (dialog));
 
