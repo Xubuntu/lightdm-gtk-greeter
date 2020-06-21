925c25279d1851d8dc87b9442d36638186d7c2c6
Sean Davis <smd.seandavis@gmail.com>
2020-06-21 05:49:44 -0400

    Refresh copyrights

--- src/greeterdeprecated.c
+++ src/greeterdeprecated.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 - 2018, Sean Davis <sean@bluesabre.org>
+ * Copyright (C) 2017 - 2020, Sean Davis <sean@bluesabre.org>
  *
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
